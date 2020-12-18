e84a4f4ee9ccee9e00acdd9bebb58285b95a89f3
David Jones <alexander.chaconas@gmail.com>
2020-12-17 20:18:04 -0500

    chapter 6 complete - left the shire

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -167,7 +167,7 @@ $("aside").append($ul)
 const leaveTheShire = () => {
   // 1. grab the hobbits (the ul in which they reside) and move them to Rivendell
       // hint: the hobbits ul is a childNode of The-Shire-- there is way to get a list of childNodes
-
+$("#Rivendell").append($("#The-Shire").find("li"))
 };
 
 // COMMIT YOUR WORK
