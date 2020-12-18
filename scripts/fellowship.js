21c1f3724a94a6a31fd4475fb84eae155c1cd6de
David Jones <alexander.chaconas@gmail.com>
2020-12-17 21:02:57 -0500

    chapter 10 complete - horn of gandor blew and Boromir is dead

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -227,11 +227,14 @@ const theBalrog = () => {
 const hornOfGondor = () => {
 
   // 1. create a pop-up alert that the horn of gondor has been blown
-
+ 
+    alert("The Horn of Gondor has been blown");
+    
   // 2. Boromir's been killed by the Uruk-hai! Put a linethrough on Boromir's name
+  $(".buddy").eq(4).html("<strike>Boromir</strike>")
 
   // 3. Tricky: Remove the Uruk-Hai from the Baddies on the page
-
+$(".baddy").eq(2).remove()
 };
 
 // COMMIT YOUR WORK
