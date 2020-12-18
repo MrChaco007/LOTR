766594bf1ab3e035fe353c07ace0e962bd9b3b70
David Jones <alexander.chaconas@gmail.com>
2020-12-17 20:47:44 -0500

    chapter 9 complete - updated gandalf

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -211,9 +211,9 @@ $div.append($("<h1>")).text("The Fellowship")
 const theBalrog = () => {
 
   // 1. change the 'Gandalf' text to 'Gandalf the White'
-
+  $(".buddy").eq(0).text("Gandalf the White")
   // 2. add a class "the-white" to this element
-
+  $(".buddy").eq(0).addClass("the-white")
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
 
 };
